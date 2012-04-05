c1072ddfcdf5ef6049b33a553999b5d217b9b8cb
renatodex <renatodex@gmail.com>
2012-04-05 20:50:03 -0300

    Upload da estrutura inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cloned::Application.config.secret_token = '5266ee03b32d6c4e477731dd32441cfea0f7ea1cbd01a40abd4665ae34d656a1a002aeffe2fa4515e1585ea37e7695842e834960bdf80666dd757aa861417503'
