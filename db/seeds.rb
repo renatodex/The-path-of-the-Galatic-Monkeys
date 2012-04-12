50567c26c160b10b25b9f64870775e37a6fe66b1
renatodex <renatodex@gmail.com>
2012-04-11 21:06:47 -0300

    Comitando tudo que ta pendente

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Emanuel', :city => cities.first)
+
+user = User.create(
+  :email                => 'renatodex@gmail.com',
+  :password   => '2410rna'
+  :password_confirmation        => '2410rna'
+)
