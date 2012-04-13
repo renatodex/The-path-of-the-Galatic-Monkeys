bead5d27722729f60fea9ce506f203d2549b9fd3
renatodex <renatodex@gmail.com>
2012-04-12 22:21:05 -0300

    Escondido campos de width desnecessarios

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -83,9 +83,9 @@ RailsAdmin.config do |config|
   config.model StoryPage do
        configure :id, :integer
        configure :nome, :string
-       #configure :wl, :integer
-       #configure :wm, :integer
-       #configure :wr, :integer
+       configure :wl, :hidden
+       configure :wm, :hidden
+       configure :wr, :hidden
        configure :titulo, :string
        configure :desc, :text
        configure :left_file_name, :string
